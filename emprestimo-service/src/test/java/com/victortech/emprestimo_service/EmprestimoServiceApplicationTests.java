fdb091f47e750530d203f92bb450b686ba15f83b
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-05 15:28:52 -0300

    initial commit

---
+++ emprestimo-service/src/test/java/com/victortech/emprestimo_service/EmprestimoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victortech.emprestimo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
